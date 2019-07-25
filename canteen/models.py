19c753ec8d1959eeed0da8d069669d560e27d4dd
Denniel Luis Sadian <sadiandenniel@gmail.com>
2019-07-25 18:45:00 +0800

    Write `Dish` model

--- canteen/models.py
+++ canteen/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+
+class Dish(model.Model):
+    name = models.CharField(max_length=20)
+    price = models.IntegerField()
+    date = models.DateField(auto_now=True)
+    estimated_count = models.IntegerField('good for how many people')
+
+    def __str__(self):
+        return self.name
