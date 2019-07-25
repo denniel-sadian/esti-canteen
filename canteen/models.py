a88feee493f789f71b903d3ae50d2c1b2240bed1
Denniel Luis Sadian <sadiandenniel@gmail.com>
2019-07-25 18:55:11 +0800

    Write `Order` model

--- canteen/models.py
+++ canteen/models.py
@@ -10,3 +10,20 @@ class Dish(model.Model):
 
     def __str__(self):
         return self.name
+
+
+class Order(model.Model):
+    date = models.DateField(auto_now=True)
+    custumer_name = models.CharField(max_length=100)
+    custumer_id_no = models.CharField(max_length=100)
+    dish = models.ForeignKey(Dish, on_delete=models.CASCADE)
+    count = models.IntegerField()
+    amount = models.FloatField()
+
+    def __str__(self):
+        return f'{self.customer_name} orderred {self.dish}'
+    
+    def save(self, *args, **kwargs):
+        self.amount = self.count * self.dish.price
+        super().save(*args, **kwargs)
+
