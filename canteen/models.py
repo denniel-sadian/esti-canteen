aa4fc4f7d932c6248e2f332cbf9dfa71e75e6dab
Denniel Luis Sadian <sadiandenniel@gmail.com>
2019-07-25 18:55:34 +0800

    Change integer to float

--- canteen/models.py
+++ canteen/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Dish(model.Model):
     name = models.CharField(max_length=20)
-    price = models.IntegerField()
+    price = models.FloatField()
     date = models.DateField(auto_now=True)
     estimated_count = models.IntegerField('good for how many people')
 
