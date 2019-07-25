0fc8b81a7cf1638ce1f591089e6a254d5acc2497
Denniel Luis Sadian <sadiandenniel@gmail.com>
2019-07-25 20:43:44 +0800

    Register the models from the cateen app

--- canteen/admin.py
+++ canteen/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Dish
+from .models import Order
+
+
+class DishAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'estimated_count',
+                    'date', 'sold_out')
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('name', 'id_no', 'dish', 'count', 'amount')
+
+
+admin.site.register(Dish, DishAdmin)
+admin.site.register(Order, OrderAdmin)
