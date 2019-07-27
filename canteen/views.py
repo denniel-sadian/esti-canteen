a3431833eb3caee60301a7c3e4794bbeed4f3cd2
Denniel Luis Sadian <sadiandenniel@gmail.com>
2019-07-27 10:50:40 +0800

    Fix the condition in the filter

--- canteen/views.py
+++ canteen/views.py
@@ -25,7 +25,7 @@ class DishView(DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['orders'] = Order.objects.filter(
-            date=datetime.now(), dish=self.object).count()
+            date__lt=datetime.now(), dish=self.object).count()
         return context
 
 
