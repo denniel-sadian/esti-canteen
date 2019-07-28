df1ee020801d0c58eab7dfc1dc0a99fef41e3dbb
Denniel Luis Sadian <sadiandenniel@gmail.com>
2019-07-28 09:07:19 +0800

    Lower the description

--- canteen/models.py
+++ canteen/models.py
@@ -18,7 +18,7 @@ class Dish(models.Model):
 class Order(models.Model):
     date = models.DateTimeField(auto_now=True)
     name = models.CharField(max_length=100)
-    id_no = models.CharField('ID NO.', max_length=100)
+    id_no = models.CharField('id no.', max_length=100)
     contact_no = models.IntegerField('contact no.')
     dish = models.ForeignKey(Dish, on_delete=models.CASCADE)
     count = models.IntegerField()
