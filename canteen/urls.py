c5b3410f8bd8a35d39d199b81df6d92bd3f5e2fe
Denniel Luis Sadian <sadiandenniel@gmail.com>
2019-07-29 19:11:31 +0800

    Add a new link for the new view

--- canteen/urls.py
+++ canteen/urls.py
@@ -12,6 +12,7 @@ urlpatterns = [
     path('feedback/', views.FeedbackView.as_view(), name='feedback'),
     path('json-orders/', views.json_orders, name='json-orders'),
     path('json-audit/', views.json_audit, name='json-audit'),
+    path('json-feedbacks/', views.json_feedbacks, name='json-feedbacks'),
     path('api-mark-as-served/<int:id>/',views.api_mark_order_served,
          name='api-served'),
     path('api-delete-order/<int:id>/', views.api_delete_order,
