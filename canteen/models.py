06013df1357baf3fc7cd715e52635b9a4d9bc72a
Denniel Luis Sadian <sadiandenniel@gmail.com>
2019-07-31 09:26:27 +0800

    Add custom save method

--- canteen/models.py
+++ canteen/models.py
@@ -21,6 +21,10 @@ class Dish(models.Model):
 
     def __str__(self):
         return self.name
+    
+    def save(self, *args, **kwargs):
+        self.name = self.name.lower()
+        super().save(*args, **kwargs)
 
 
 class Feedback(models.Model):
