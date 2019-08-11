62efb768fef1af5c7e771c3ab1fdfdb13540b82a
Denniel Luis Sadian <sadiandenniel@gmail.com>
2019-08-11 10:19:39 +0800

    Add permissions

--- canteen/api_views.py
+++ canteen/api_views.py
@@ -1,4 +1,5 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 from .serializers import DishSerializer
 from .serializers import OrderSerializer
@@ -9,5 +10,6 @@ from .models import Feedback
 
 
 class DishViews(ModelViewSet):
+    permission_classes = (IsAuthenticatedOrReadOnly,)
     queryset = Dish.objects.all()
     serializer_class = DishSerializer
