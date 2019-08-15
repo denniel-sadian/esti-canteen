9275bb3af98be37ea12e0ca63bbcdfb4521f31fb
Denniel Luis Sadian <sadiandenniel@gmail.com>
2019-08-15 20:54:37 +0800

    Remove date hierarchy

--- canteen/admin.py
+++ canteen/admin.py
@@ -7,7 +7,6 @@ from .models import Feedback
 
 
 class DishAdmin(admin.ModelAdmin):
-    date_hierarchy = 'date'
     list_display = ('name', 'price', 'description', 'date', 'sold_out')
 
     def get_queryset(self, request):
