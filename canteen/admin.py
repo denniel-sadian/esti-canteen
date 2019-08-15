2388d6efae88a1809127609b84ed83ac5a919cdf
Denniel Luis Sadian <sadiandenniel@gmail.com>
2019-08-15 20:39:53 +0800

    Revert

--- canteen/admin.py
+++ canteen/admin.py
@@ -5,17 +5,19 @@ from .models import Order
 from .models import Feedback
 
 
-@admin.site.register(Dish)
 class DishAdmin(admin.ModelAdmin):
     list_display = ('name', 'price', 'description', 'date', 'sold_out')
 
 
-@admin.site.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = ('name', 'dish', 'count', 'amount', 'id_no',
                     'contact_no', 'date')
 
 
-@admin.site.register(Feedback)
 class FeedbackAdmin(admin.ModelAdmin):
     list_display = ('name', 'contact_no', 'date')
+
+
+admin.site.register(Dish, DishAdmin)
+admin.site.register(Order, OrderAdmin)
+admin.site.register(Feedback, FeedbackAdmin)
