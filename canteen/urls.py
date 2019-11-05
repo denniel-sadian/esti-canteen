c2ab2c8b5db1b28c271d531d894f233df539eac0
Denniel Luis Sadian <sadiandenniel@gmail.com>
2019-11-05 22:30:14 +0800

    Re-arrange the urls

--- canteen/urls.py
+++ canteen/urls.py
@@ -25,8 +25,8 @@ urlpatterns = [
     
     path('manage/', views.ManageView.as_view(), name='manage'),
     path('manage/create/', views.CreateDishView.as_view(), name='create'),
-    path('manage/edit/<int:pk>/', views.UpdateDishView.as_view(), name='edit'),
     path('manage/edit-order/<int:pk>/', views.UpdateOrderView.as_view(), name='edit-order'),
+    path('manage/edit/<int:pk>/', views.UpdateDishView.as_view(), name='edit'),
     path('login/', LoginView.as_view(
         template_name='canteen/login.html',
     )),
