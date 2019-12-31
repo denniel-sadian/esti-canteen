e0df473463808f4ef431e3535ef66e1fa624b67f
Denniel Luis Sadian <sadiandenniel@gmail.com>
2019-12-31 20:16:22 +0800

    Update

--- canteen/consumers.py
+++ canteen/consumers.py
@@ -23,23 +23,19 @@ class ReportConsumer(AsyncWebsocketConsumer):
 
     # Receive message from WebSocket
     async def receive(self, text_data):
-        text_data_json = json.loads(text_data)
-        message = text_data_json['message']
 
         # Send message to room group
         await self.channel_layer.group_send(
             self.group_name,
             {
-                'type': 'chat_message',
-                'message': 'message'
+                'type': 'message',
+                'message': ''
             }
         )
 
     # Receive message from room group
-    async def chat_message(self, event):
-        message = event['message']
-
+    async def message(self, event):
         # Send message to WebSocket
         await self.send(text_data=json.dumps({
-            'message': 'message'
+            'message': ''
         }))
