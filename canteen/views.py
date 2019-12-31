d4bb83e03c952a3d09695742f4bd6f29f9571443
Denniel Luis Sadian <sadiandenniel@gmail.com>
2019-12-31 23:18:32 +0800

    Make them asynchronous

--- canteen/views.py
+++ canteen/views.py
@@ -110,8 +110,11 @@ class OrderView(CreateView):
     def form_valid(self, form):
         form.instance.dish = Dish.objects.get(id=self.kwargs['dish'])
         form.save()
-        self.request.session['orders'] = (
-            self.request.session.get('orders').append(form.instance.id))
+        if type(self.request.session.get('orders')) != list:
+            self.request.session['orders'] = []
+        orders = self.request.session.get('orders')
+        orders.append(form.instance.id)
+        self.request.session['orders'] = orders
         return super().form_valid(form)
     
     def get_context_data(self, **kwargs):
