7be83eaeb33e7962ab3a8ecfbacd59f561a74bb3
Denniel Luis Sadian <sadiandenniel@gmail.com>
2019-12-31 18:39:14 +0800

    Add the websockets

--- canteen/consumers.py
+++ canteen/consumers.py
@@ -1,15 +1,45 @@
-from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import AsyncWebsocketConsumer
 import json
 
 
-class ReportConsumer(WebsocketConsumer):
-    def connect(self):
-        self.accept()
+class ReportConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.group_name = 'report'
 
-    def disconnect(self, close_code):
-        pass
+        # Join room group
+        await self.channel_layer.group_add(
+            self.group_name,
+            self.channel_name
+        )
 
-    def receive(self, text_data):
-        self.send(text_data=json.dumps({
-            'update': 'update'
+        await self.accept()
+
+    async def disconnect(self, close_code):
+        # Leave room group
+        await self.channel_layer.group_discard(
+            self.group_name,
+            self.channel_name
+        )
+
+    # Receive message from WebSocket
+    async def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        message = text_data_json['message']
+
+        # Send message to room group
+        await self.channel_layer.group_send(
+            self.group_name,
+            {
+                'type': 'chat_message',
+                'message': 'message'
+            }
+        )
+
+    # Receive message from room group
+    async def chat_message(self, event):
+        message = event['message']
+
+        # Send message to WebSocket
+        await self.send(text_data=json.dumps({
+            'message': 'message'
         }))
--- esticanteen/settings.py
+++ esticanteen/settings.py
@@ -168,4 +168,14 @@ THUMBNAIL_FORCE_OVERWRITE = True
 # CHANNELS
 
 SESSION_SAVE_EVERY_REQUEST = True
+
 ASGI_APPLICATION = 'esticanteen.routing.application'
+
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [('127.0.0.1', 6379)],
+        },
+    },
+}
