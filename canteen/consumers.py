9653c8d06c7f64b48d27f35b60349c23578cae1f
Denniel Luis Sadian <sadiandenniel@gmail.com>
2020-01-01 21:37:38 +0800

    Remove comments

--- canteen/consumers.py
+++ canteen/consumers.py
@@ -6,7 +6,6 @@ class ReportConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.group_name = 'report'
 
-        # Join room group
         await self.channel_layer.group_add(
             self.group_name,
             self.channel_name
@@ -15,7 +14,6 @@ class ReportConsumer(AsyncWebsocketConsumer):
         await self.accept()
 
     async def disconnect(self, close_code):
-        # Leave room group
         await self.channel_layer.group_discard(
             self.group_name,
             self.channel_name
@@ -24,7 +22,7 @@ class ReportConsumer(AsyncWebsocketConsumer):
     # Receive message from WebSocket
     async def receive(self, text_data):
 
-        # Send message to room group
+        # Send message
         await self.channel_layer.group_send(
             self.group_name,
             {
@@ -33,7 +31,7 @@ class ReportConsumer(AsyncWebsocketConsumer):
             }
         )
 
-    # Receive message from room group
+    # Receive message from group
     async def message(self, event):
         # Send message to WebSocket
         await self.send(text_data=json.dumps({
