abac72e6bd06fa22ef1531c2df0902954f9655f0
Denniel Luis Sadian <sadiandenniel@gmail.com>
2020-01-02 15:07:10 +0800

    Updated the signals

--- canteen/signals.py
+++ canteen/signals.py
@@ -1,9 +1,25 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from asgiref.sync import async_to_sync
+import channels.layers
 
 from .models import Order
+
+channel_layer = channels.layers.get_channel_layer()
+
+
+def message(self, event):
+    async_to_sync(self.send(text_data=json.dumps({
+        'message': ''
+    })))
 
 
 @receiver(post_save, sender=Order)
 def notify_report(sender, **kwargs):
-    print('Order here.')
+    async_to_sync(channel_layer.group_send(
+        'report',
+        {
+            'type': 'message',
+            'message': ''
+        }
+    ))
