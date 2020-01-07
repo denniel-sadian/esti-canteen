ec5c5c998c3bcbd3a7373ed602b8fb9471346685
Denniel Luis Sadian <sadiandenniel@gmail.com>
2020-01-07 19:44:08 +0800

    Update

--- esticanteen/wsgi.py
+++ esticanteen/wsgi.py
@@ -7,9 +7,13 @@ For more information on this file, see
 https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
+from dotenv import load_dotenv
+
 import os
 
 from django.core.wsgi import get_wsgi_application
+
+load_dotenv(os.path.join(path, '.env'))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'esticanteen.settings')
 
