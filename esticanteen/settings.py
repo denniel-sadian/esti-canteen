f4bf874a1c207e796dfe4a1a66ad9dda38725e9b
Denniel Luis Sadian <sadiandenniel@gmail.com>
2020-01-12 10:27:07 +0800

    Add setting

--- esticanteen/settings.py
+++ esticanteen/settings.py
@@ -183,8 +183,17 @@ GITHUB_REPO_NAME = 'esticanteen-bucket'
 SESSION_ENGINE = 'django.contrib.sessions.backends.signed_cookies'
 
 
+# SSL
+
+SECURE_SSL_REDIRECT = True
+
+if DEBUG:
+    SECURE_SSL_REDIRECT = False
+
+
 # Activate Django-Heroku.
 django_heroku.settings(locals())
+
 
 if not DEBUG:
     del DATABASES['default']['OPTIONS']['sslmode']
