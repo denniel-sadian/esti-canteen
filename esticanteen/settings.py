dfdc34636b2ed9e419e15203c729c71f2d29a147
Denniel Luis Sadian <sadiandenniel@gmail.com>
2020-01-19 21:41:06 +0800

    Ok

--- esticanteen/settings.py
+++ esticanteen/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'esticanteen.pythonanywhere.com',
@@ -187,4 +187,5 @@ SESSION_ENGINE = 'django.contrib.sessions.backends.signed_cookies'
 django_heroku.settings(locals())
 
 
-del DATABASES['default']['OPTIONS']['sslmode']
+if not DEBUG:
+    del DATABASES['default']['OPTIONS']['sslmode']
